e01b3258b585298135834557a6c86eba274af7d6
lilian <jl_raining@163.com>
2021-09-24 17:27:12 +0800

    feat: add leetcode 3

--- leetcode-algorithm/16-sliding-window && double pointer/medium-15-3Sum.ts
+++ leetcode-algorithm/16-sliding-window && double pointer/medium-15-3Sum.ts
@@ -11,35 +11,39 @@
 // Output: []
 
 
+/**
+ * solution:
+ * order + double pointer
+ */
 function threeSum(nums: number[]): number[][] {
     let res: Array<number[]> = [];
     if (!nums || (nums && nums.length < 3)) {
         return [];
-      }
-      const arr = nums.sort((a, b) => a - b);
-      for (let i = 0; i < arr.length; i++) {
+    }
+    const arr = nums.sort((a, b) => a - b);
+    for (let i = 0; i < arr.length; i++) {
         if (arr[i] === arr[i - 1]) {
-          continue;
+            continue;
         }
-        let p1 = i + 1; 
-        let p2 = arr.length - 1;
+        let p1: number = i + 1;
+        let p2: number = arr.length - 1;
         while (p2 > p1) {
-          if (arr[i] === -(arr[p1] + arr[p2])) {
-            res.push([arr[i], arr[p1], arr[p2]]);
-            while (arr[p1] === arr[p1 + 1]) {
-              p1++;
+            if (arr[i] === -(arr[p1] + arr[p2])) {
+                res.push([arr[i], arr[p1], arr[p2]]);
+                while (arr[p1] === arr[p1 + 1]) {
+                    p1++;
+                }
+                while (arr[p2] === arr[p2 + 1]) {
+                    p2--;
+                }
+                p1++;
+                p2--;
+            } else if (arr[i] > -(arr[p1] + arr[p2])) {
+                p2--;
+            } else {
+                p1++;
             }
-            while (arr[p2] === arr[p2 + 1]) {
-              p2--;
-            }
-            p1++;
-            p2--;
-          } else if (arr[i] > -(arr[p1] + arr[p2])) {
-            p2--;
-          } else {
-            p1++;
-          }
         }
-      }
+    }
     return res;
 };
---
+++ leetcode-algorithm/16-sliding-window && double pointer/medium-3-Longest Substring Without Repeating Characters.ts
@@ -0,0 +1,48 @@
+// medium-3-Longest Substring Without Repeating Characters.ts
+// Given a string s, find the length of the longest substring without repeating characters.
+// 
+// Example 1:
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+// Example 2:
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+// Example 4:
+// Input: s = ""
+// Output: 0
+
+/**
+ * solution:
+ * use map && sliding window
+ */
+
+ function lengthOfLongestSubstring2(s: string): number {
+    if (!s) {
+        return 0;
+    }
+
+    let p1: number = 0;
+    let p2: number = 0;
+    let map: Map<string, number> = new Map();
+    let res: number = 1;
+    map.set(s[0], 0);
+
+    while (p2 < s.length - 1) {
+        p2++;
+        // this condition is vital
+        if (map.has(s[p2]) && map.get(s[p2]) >= p1) {
+            p1 = map.get(s[p2]) + 1;
+        }
+        map.set(s[p2], p2);
+        res = Math.max(res, p2 - p1 + 1);
+    }
+
+    return res;
+};
