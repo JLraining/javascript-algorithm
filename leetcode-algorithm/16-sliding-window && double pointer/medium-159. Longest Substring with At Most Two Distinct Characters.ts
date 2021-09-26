3b901a7f0f04d35fd8c9026ff34cf6a9771cd3d2
lilian <jl_raining@163.com>
2021-09-26 21:28:50 +0800

    feat: add leetcode 159

---
+++ leetcode-algorithm/16-sliding-window && double pointer/medium-159. Longest Substring with At Most Two Distinct Characters.ts
@@ -0,0 +1,43 @@
+// Given a string s, return the length of the longest substring that contains at most two distinct characters.
+
+//  
+
+// Example 1:
+
+// Input: s = "eceba"
+// Output: 3
+// Explanation: The substring is "ece" which its length is 3.
+// Example 2:
+
+// Input: s = "ccaabbb"
+// Output: 5
+// Explanation: The substring is "aabbb" which its length is 5.
+
+
+/**
+ * solution
+ * sliding window
+ */
+function lengthOfLongestSubstringTwoDistinct(s: string): number {
+    if (!s) {
+        return 0;
+    }
+    let p1: number = 0;
+    let p2: number = 1;
+    let res: number = 1;
+    let preS: [string, number] = ['', -1];
+
+    while (p2 < s.length) {
+        if (s[p2] !== s[p2 - 1]) {
+            if (!preS[0] || s[p2] === preS[0]) {
+                preS = [s[p2 - 1], p2 - 1]
+            } else {
+                p1 = preS[1] + 1;
+                preS = [s[p2 - 1], p2 - 1];
+            }
+        }
+        res = Math.max(res, p2 - p1 + 1)
+        p2++;
+    }
+    return res;
+};
