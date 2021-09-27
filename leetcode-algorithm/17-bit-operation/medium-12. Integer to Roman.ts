3c0d9a5acaebe6a2b5c9402c7d28920cfb35013c
lilian <jl_raining@163.com>
2021-09-27 23:09:57 +0800

    feat: add leetcode 12

---
+++ leetcode-algorithm/17-bit-operation/medium-12. Integer to Roman.ts
@@ -0,0 +1,50 @@
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+// For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+// Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+// I can be placed before V (5) and X (10) to make 4 and 9. 
+// X can be placed before L (50) and C (100) to make 40 and 90. 
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+// Given an integer, convert it to a roman numeral.
+
+// Example 1:
+// Input: num = 3
+// Output: "III"
+// Example 2:
+// Input: num = 4
+// Output: "IV"
+// Example 3:
+// Input: num = 9
+// Output: "IX"
+// Example 4:
+// Input: num = 58
+// Output: "LVIII"
+// Explanation: L = 50, V = 5, III = 3.
+// Example 5:
+// Input: num = 1994
+// Output: "MCMXCIV"
+// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+
+function intToRoman(num: number): string {
+    let hashMap: Array<string[]> = [
+        ['', "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+        ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+        ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
+        ["", "M", "MM", "MMM"]
+    ];
+    
+    let one_digit: number = num % 10;
+    let ten_digit: number  = Math.floor((num % 100) / 10);
+    let hundred_digit: number  = Math.floor((num % 1000) / 100);
+    let thousand_digit: number  = Math.floor(num / 1000);
+
+    return hashMap[3][thousand_digit] + hashMap[2][hundred_digit] + hashMap[1][ten_digit] + hashMap[0][one_digit];
+};
