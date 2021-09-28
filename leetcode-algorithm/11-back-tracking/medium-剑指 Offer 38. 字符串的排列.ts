c156ce8dc1b6b5524f69256d08381689bc89e332
lilian <jl_raining@163.com>
2021-09-28 21:53:31 +0800

    feat: add 剑指offer 38

---
+++ leetcode-algorithm/11-back-tracking/medium-剑指 Offer 38. 字符串的排列.ts
@@ -0,0 +1,32 @@
+// 输入一个字符串，打印出该字符串中字符的所有排列。
+// 你可以以任意顺序返回这个字符串数组，但里面不能有重复元素。
+
+// 示例:
+// 输入：s = "abc"
+// 输出：["abc","acb","bac","bca","cab","cba"]
+
+
+/**
+ * solution：
+ * use back tracking algorithm
+ * pay attention to the situation that has duplicate elements.
+ */
+
+function permutation(s: string): string[] {
+    let res: string[] = [];
+    let arr: string[] = s.split("").sort();
+    const backTracking = (path: string, cur: string[]): void => {
+        if(path.length === s.length) {
+            res.push(path);
+            return;
+        }
+        for(let i: number = 0; i < cur.length; i++) {
+            if(i > 0 && cur[i] === cur[i - 1]) {
+                continue;
+            }
+           backTracking(path + cur[i], [...cur.slice(0,i), ...cur.slice(i+1)]) 
+        }
+    }
+    backTracking('', arr)
+    return res;
+};
