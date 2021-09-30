457217a3dd2988fe163b26065ddea1c4b11fc200
lilian <jl_raining@163.com>
2021-09-30 23:23:54 +0800

    feat: add leetcode 43

---
+++ leetcode-algorithm/17-bit-operation/medium-43. Multiply Strings.ts
@@ -0,0 +1,17 @@
+// Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+// Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+// Example 1:
+// Input: num1 = "2", num2 = "3"
+// Output: "6"
+// Example 2:
+// Input: num1 = "123", num2 = "456"
+// Output: "56088"
+
+/**
+ * [hot leetcode question]
+ * solution:
+ * 
+ */
+function multiply(num1: string, num2: string): string {
+
+};
