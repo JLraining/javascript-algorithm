4bc0d4f77e48fa0079319f608f866c02f44388e0
lilian <jl_raining@163.com>
2021-10-01 21:33:50 +0800

    feat: add leetcode 43

--- leetcode-algorithm/17-bit-operation/medium-43. Multiply Strings.ts
+++ leetcode-algorithm/17-bit-operation/medium-43. Multiply Strings.ts
@@ -10,8 +10,23 @@
 /**
  * [hot leetcode question]
  * solution:
- * 
+ * use res array to record every bit postion value.
+ * pay attention to the situation that num1 or num3 equal to zero
  */
-function multiply(num1: string, num2: string): string {
+ function multiply(num1: string, num2: string): string {
+    let res: number[] = new Array(num1.length + num2.length).fill(0);
 
+    for(let i: number = num2.length - 1; i >= 0; i--) {
+        for(let j: number = num1.length - 1; j >= 0; j--) {
+            let n: number = Number(num1[j]) * Number(num2[i]) + res[i + j + 1] ;
+            res[i + j + 1] = n % 10;
+            res[i + j] = Math.floor(n / 10) + res[i + j];
+        }
+    }
+
+    while (res[0] == 0) {
+        res.shift();
+    }
+
+    return res.length ? res.join('') : '0'
 };
