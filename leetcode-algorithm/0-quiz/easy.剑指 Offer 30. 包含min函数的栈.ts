5b48a3dc18a73f39f38b279276f4e5a9b85caa3c
lilian <jl_raining@163.com>
2021-10-12 10:23:45 +0800

    feat: add leetcode

---
+++ leetcode-algorithm/0-quiz/easy.剑指 Offer 30. 包含min函数的栈.ts
@@ -0,0 +1,43 @@
+// 定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的 min 函数在该栈中，调用 min、push 及 pop 的时间复杂度都是 O(1)。
+//  
+// 示例:
+// MinStack minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// minStack.min();   --> 返回 -3.
+// minStack.pop();
+// minStack.top();      --> 返回 0.
+// minStack.min();   --> 返回 -2.
+//  
+// 提示：
+// 各函数的调用总次数不超过 20000 次
+
+
+/**
+ * 非常巧妙的方法 辅助队列
+ * https://leetcode-cn.com/problems/bao-han-minhan-shu-de-zhan-lcof/solution/offer30bao-han-minhan-shu-de-zhan-by-log-a6vx/
+ */
+
+var MinStack = function() {
+    this.x_stack = [];
+    this.min_stack = [Infinity];
+};
+
+MinStack.prototype.push = function(x) {
+    this.x_stack.push(x);
+    this.min_stack.push(Math.min(this.min_stack[this.min_stack.length - 1], x));
+};
+
+MinStack.prototype.pop = function() {
+    this.x_stack.pop();
+    this.min_stack.pop();
+};
+
+MinStack.prototype.top = function() {
+    return this.x_stack[this.x_stack.length - 1];
+};
+
+MinStack.prototype.min = function() {
+    return this.min_stack[this.min_stack.length - 1];
+};
