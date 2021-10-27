424a8569ab105e4491548eb81c48420f1e4a0320
lilian <jl_raining@163.com>
2021-10-27 23:32:29 +0800

    feat: add offer2 095

--- leetcode-algorithm/14-dynamic-program/medium-剑指 Offer II 095. 最长公共子序列.ts
+++ leetcode-algorithm/14-dynamic-program/medium-剑指 Offer II 095. 最长公共子序列.ts
@@ -16,8 +16,25 @@
 // 解释：两个字符串没有公共子序列，返回 0 。
 
 /**
- * [重要] 最长公共子序列问题是典型的二维动态规划问题
- *
+ * [重要] 最长公共子序列问题是 典型的二维动态规划问题
+ * https://leetcode-cn.com/problems/qJnOS7/solution/zui-chang-gong-gong-zi-xu-lie-by-leetcod-ugg7/
+ * 时间复杂度：O(mn)
+ * 空间复杂度：O(mn)
  */
 
+ function longestCommonSubsequence(text1: string, text2: string): number {
+    let dn: number[][] = new Array(text1.length + 1).fill(0).map(() => new Array(text2.length + 1).fill(0));
 
+    for(let i: number = 1; i <= text1.length; i++) {
+        for(let j: number = 1; j <= text2.length; j++) {
+            if(text1[i - 1] === text2[j - 1]) {
+                dn[i][j] = dn[i - 1][j - 1] + 1;
+            } else {
+                dn[i][j] = Math.max(dn[i - 1][j], dn[i][j - 1])
+            }
+           
+        }
+    }
+
+    return dn[text1.length][text2.length];
+};
